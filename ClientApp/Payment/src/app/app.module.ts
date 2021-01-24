d24a2f75fa9573a617652cd771cb63463e6486f4
surkovitalii <vitalkya40@gmail.com>
2021-01-24 20:50:44 +0200

    first commit

---
+++ ClientApp/Payment/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import{FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailsFormComponent } from './payment-details/payment-details-form/payment-details-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailsFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ClientApp/Payment/src/app/payment-details/payment-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { PaymentDetail } from '../shared/payment-detail.model';
+import { PaymentDetailService } from '../shared/payment-detail.service';
+
+@Component({
+  selector: 'app-payment-details',
+  templateUrl: './payment-details.component.html',
+  styleUrls: ['./payment-details.component.css']
+})
+export class PaymentDetailsComponent implements OnInit {
+
+  constructor(public service:PaymentDetailService,
+    private toastr:ToastrService) { }
+
+  ngOnInit(): void {
+    this.service.refreshList();
+  }
+ populateForm(selectedRecord:PaymentDetail)
+ {
+   this.service.formData=Object.assign({},selectedRecord);
+ }
+ onDelete(id:number)
+ {
+  if(confirm('Are you sure to delete this record?')){
+  this.service.deletePaymentDetail(id).subscribe(
+  res=>{
+    this.service.refreshList();
+    this.toastr.error("Deleted successfully",'Payment Detail Register');
+  },
+  err=>{console.log(err)}
+)
+}
+ }
+}
